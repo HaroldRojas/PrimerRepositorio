20a0797e3b47b4defb56d2accda71f1c5284d315
Emilio Rojas <Emilio Rojas@10.68.173.49>
2015-03-13 13:36:11 -0600

    PrimeraSubida

---
+++ PrimerProyecto/src/primerproyecto/PrimerProyecto.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerproyecto;
+
+/**
+ *
+ * @author Emilio Rojas
+ */
+public class PrimerProyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //INSERTE COMENTARIO AQUÍ....
+        System.out.println("Quien te conoce texto");
+    }
+    
+}
